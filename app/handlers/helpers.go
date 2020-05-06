c2ca73b7f17ae313883151b3c1337a73fd075edd
kczechowski <karolcz97@gmail.com>
2020-05-06 22:15:43 +0200

    Added helper that writes json to response

--- app/handlers/helpers.go
+++ app/handlers/helpers.go
@@ -1,1 +1,18 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func respondWithJson(w http.ResponseWriter, payload interface{}) http.ResponseWriter{
+	body, err := json.Marshal(payload)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return w
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(body))
+	return w
+}
